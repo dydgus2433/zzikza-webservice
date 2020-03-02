ba3f66127cb6fff2871aade7bc09fdbc8dee0374
leeyonghyun <dydgus2433@naver.com>
2020-03-02 20:50:35 +0900

    공용시간엔티티 BaseTimeEntity 추가

--- src/main/java/com/zzikza/springboot/domain/posts/Posts.java
+++ src/main/java/com/zzikza/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.zzikza.springboot.domain.posts;
 
+import com.zzikza.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
